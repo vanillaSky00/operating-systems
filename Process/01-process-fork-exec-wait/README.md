7e0be0279bab95f69e78e3fdb1615e1533765c94
vanillaSky00 <pipichun2@gmail.com>
2025-09-28 01:32:05 +0800

    fork tree but some api is os-dependent

---
+++ Process/01-process-fork-exec-wait/README.md
@@ -0,0 +1,16 @@
+
+## Overall
+
+
+```
+      A
+     / \
+    B   C
+     \
+      D
+     / \
+    E   F
+```
+
+## Reference 
+https://stackoverflow.com/questions/73936920/how-to-make-a-process-tree-in-c
---
+++ Process/01-process-fork-exec-wait/fork_tree.c
@@ -0,0 +1,48 @@
+#define _XOPEN_SOURCE 700
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <errno.h>
+
+void B(pid_t parent) {
+    printf("B: pid=%d, ppid=%d\n", getpid(), parent);
+    pause(); // wait for signal
+}
+
+void C(pid_t parent) {
+    printf("C: pid=%d, ppid=%d\n", getpid(), parent);
+    pause(); // wait for signal
+}
+
+int main(void) {
+    pid_t A_pid = getpid();
+    pid_t B_pid = fork();
+    pid_t C_pid = 0;
+
+    if (B_pid == 0) { 
+        B(A_pid); // in B
+    }
+    else if (B_pid > 0) {
+        C_pid = fork();
+
+        if (C_pid == 0) {
+            C(A_pid); // in C
+        }
+        else if (C_pid > 0) {
+            printf("I am process A. My PID is %d and I don't have a parent\n",A_pid);
+        }
+        else {
+
+        }
+    }
+
+    
+    //sleep(1);                    // pretend to do work
+    kill(B_pid, SIGTERM);        // terminate children
+    kill(C_pid, SIGTERM);
+
+    return 0;
+}
