44bfce05ab5457d5fe3890f86b6f417cf15f7446
vanilasky00 <pipichun@gmail.com>
2025-09-28 01:59:04 +0800

    mvp for process tree, need to hanlde error and other code style

--- Process/01-process-fork-exec-wait/fork_tree.c
+++ Process/01-process-fork-exec-wait/fork_tree.c
@@ -1,4 +1,4 @@
-#define _XOPEN_SOURCE 700
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,14 +7,44 @@
 #include <signal.h>
 #include <errno.h>
 
+void D(pid_t parent) {
+    printf("D: pid=%d, ppid=%d\n", getpid(), parent);
+
+    pid_t D_pid = getpid();
+    pid_t F_pid = fork();
+    pid_t E_pid = 0;
+    
+    if (F_pid == 0) {
+        printf("F: pid=%d, ppid=%d\n", getpid(), D_pid);
+    }
+    else if (F_pid > 0) {
+        E_pid = fork();
+        if (E_pid == 0) {
+            printf("E: pid=%d, ppid=%d\n", getpid(), D_pid);
+        }
+        else if (E_pid > 0) {
+            // in process D
+        }
+    }
+}
+
 void B(pid_t parent) {
     printf("B: pid=%d, ppid=%d\n", getpid(), parent);
-    pause(); // wait for signal
+    //pause(); // wait for signal
+    pid_t B_pid = getpid();
+    pid_t D_pid = fork();
+
+    if (D_pid == 0) {
+        D(B_pid);
+    }
+    else if (D_pid > 0) {
+        // in process B
+    }
 }
 
 void C(pid_t parent) {
     printf("C: pid=%d, ppid=%d\n", getpid(), parent);
-    pause(); // wait for signal
+    //pause(); // wait for signal
 }
 
 int main(void) {
@@ -35,14 +65,13 @@ int main(void) {
             printf("I am process A. My PID is %d and I don't have a parent\n",A_pid);
         }
         else {
-
+            // error 
         }
     }
 
-    
-    //sleep(1);                    // pretend to do work
-    kill(B_pid, SIGTERM);        // terminate children
-    kill(C_pid, SIGTERM);
+    sleep(1);                    // pretend to do work
+    //kill(B_pid, SIGTERM);        // terminate children
+    //kill(C_pid, SIGTERM);
 
     return 0;
 }
